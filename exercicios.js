8a0324ded9299d50b2ab7d705205967db1fe9c75
GIOVANNA VIEIRA DE SOUZA <giovanna_v_souza@estudante.sesisenai.org.br>
2026-08-17 20:38:31 -0300

    Fiz a 5

--- exercicios.js
+++ exercicios.js
@@ -98,8 +98,14 @@ else{
 // Se não, mostre "Menor de idade".
 // ------------------------------------------------------------
 
+let idade = 13
 
-
+if (idade >= 18){
+    console.log("Maior de idade, Você pode beber!")
+}
+else{
+    console.log("Menor de idade! Não beba e nem dirija!")
+}
 
 // EXERCÍCIO 6 - Calculadora de IMC
 // Crie variáveis para peso (em kg) e altura (em metros).
